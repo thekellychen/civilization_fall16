22bcf58f6e48ef9cbb2865bbd0b0d4da7effee4e
Kelly Chen <Kelly@lawn-128-61-121-27.lawn.gatech.edu>
2016-09-22 21:48:53 -0400

    initial commit Game.java

---
+++ Game.java
@@ -0,0 +1,15 @@
+public class Game {
+    private int healthIncrease;
+
+    public Game(int health) {
+        this.healthIncrease = healthIncrease;
+    }
+
+    public Game() {
+        this.healthIncrease = 20;
+    }
+
+    public int getHealth() {
+        return healthIncrease;
+    }
+}

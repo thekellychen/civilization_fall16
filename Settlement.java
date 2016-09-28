2485990bb6c8561dd6fb032a9097c54094a4fcda
Kelly Chen <Kelly@lawn-128-61-121-27.lawn.gatech.edu>
2016-09-28 17:56:23 -0400

    initial commit Settlement.java

---
+++ Settlement.java
@@ -0,0 +1,36 @@
+public class Settlement {
+    private Building[] buildings;
+    private String name;
+    private int numBuildings;
+
+    public Settlement(Building[] buildings, String name) {
+        this.name = name;
+        this.buildings = new Building[10];
+        this.numBuildings = 0;
+    }
+
+    private void addBuilding(Building building) {
+        numBuildings++;
+        if (numBuildings > buildings.length) {
+            expandSettlement();
+        }
+        buildings[numBuildings - 1] = building;
+    }
+
+    public boolean build(int allottedMoney, Population population, int cost, int workersRequired) {
+        if ((cost <= allottedMoney) && population.canWork(workersRequired)) {
+            Building building = new Building(cost, workersRequired);
+            addBuilding(building);
+            return true;
+        }
+        return false;
+    }
+
+    public void expandSettlement() {
+        Building[] tempArray =  new Building[buildings.length * 2];
+        for (int i = 0; i < buildings.length; i++) {
+            tempArray[i] = buildings[i];
+        }
+        buildings = tempArray;
+    }
+}
